857f48efa9a0130417c6a044bcf1effc583b190f
Quentin G <quentin01960@orange.fr>
2018-04-02 21:28:02 +0200

    [FEAT] added tool bar title

--- app/src/main/java/fr/iut/iem/pokecard/ui/fragment/GiftFragment.kt
+++ app/src/main/java/fr/iut/iem/pokecard/ui/fragment/GiftFragment.kt
@@ -3,7 +3,6 @@ package fr.iut.iem.pokecard.ui.fragment
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,7 +15,7 @@ import fr.iut.iem.pokecard.ui.listener.MainNavigatorListener
 import fr.iut.iem.pokecard.ui.listener.PokedexItemListener
 import fr.iut.iem.pokecard.ui.presenter.GiftPresenter
 import fr.iut.iem.pokecard.ui.view.PokedexView
-import kotlinx.android.synthetic.main.fragment_gift.view.*
+import kotlinx.android.synthetic.main.fragment_gift.*
 import kotlinx.android.synthetic.main.poke_toolbar.*
 
 /**
@@ -29,7 +28,7 @@ class GiftFragment : Fragment(), PokedexItemListener, PokedexView {
         private const val USER_NAME_KEY = "pseudo"
         private const val USER_ID_KEY = "id"
 
-        fun newInstance(user: User) : GiftFragment {
+        fun newInstance(user: User): GiftFragment {
             val args = Bundle()
             args.putString(GiftFragment.USER_NAME_KEY, user.pseudo)
             args.putInt(GiftFragment.USER_ID_KEY, user.id!!)
@@ -41,47 +40,50 @@ class GiftFragment : Fragment(), PokedexItemListener, PokedexView {
         }
     }
 
-    private lateinit var presenter : GiftPresenter
-    private lateinit var adapter : PokedexAdapter
+    private lateinit var presenter: GiftPresenter
+    private lateinit var adapter: PokedexAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_gift, container, false)
+        return inflater.inflate(R.layout.fragment_gift, container, false)
+    }
 
-        initUI(view)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        initRecyclerView(view)
+        initUI()
+        initRecyclerView()
 
         presenter = GiftPresenter(this)
         presenter.getCurrentUser()
-
-        return view
     }
 
     override fun updateUI(pokemons: List<Pokemon>) {
-        this.view!!.fragment_gift_loader.visibility = View.GONE
+        fragment_gift_loader.visibility = View.GONE
         poke_toolbar.setNavigationOnClickListener { activity!!.onBackPressed() }
         adapter.setPokedex(pokemons)
     }
 
     override fun onGiftSucceed() {
-        this.view!!.fragment_gift_loader.visibility = View.GONE
+        fragment_gift_loader.visibility = View.GONE
         Toast.makeText(context, "Votre Pokémon a bien été envoyé !", Toast.LENGTH_SHORT).show()
         (this.activity as MainNavigatorListener).launchUserPokedexView()
     }
 
-    private fun initRecyclerView(view : View) {
+    private fun initRecyclerView() {
         adapter = PokedexAdapter(this)
-        view.fragment_gift_list.layoutManager = LinearLayoutManager(context)
-        view.fragment_gift_list.adapter = adapter
+        fragment_gift_list.layoutManager = LinearLayoutManager(context)
+        fragment_gift_list.adapter = adapter
+        fragment_gift_list
     }
 
-    private fun initUI(view: View) {
+    private fun initUI() {
         val userName = this.arguments!![USER_NAME_KEY] as String
-        view.fragment_gift_informations_text.text = "Quel Pokemon voulez-vous envoyer à " + userName + " ?"
+        fragment_gift_informations_text.text = String.format(resources.getString(R.string.fragment_gift_information), userName)
+        poke_toolbar.title = resources.getString(R.string.fragment_gift_toolbar_title)
     }
 
     override fun onClickOnPokemon(id: Int) {
-        this.view!!.fragment_gift_loader.visibility = View.VISIBLE
+        fragment_gift_loader.visibility = View.VISIBLE
         presenter.sendGift(this.arguments!![USER_ID_KEY] as Int, id)
     }
 
